5ac1adba0c9f13cceb67ea22e622ea7a62e474f3
Neev100 <79758801+Neev100@users.noreply.github.com>
2021-11-15 20:24:02 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+img = "";
+status = "";
+
+function bedroom() {
+    img = loadImage("bedroom.jpg");
+}
+function bathroom() {
+    img = loadImage("bathroom.jpg");
+}
+function AC() {
+    img = loadImage("ac.jpg");
+}
+function TV() {
+    img = loadImage("tv.jpg");
+}
+function kitchen() {
+    img = loadImage("kitchen.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error();
+    }
+    else {
+        console.log(results);
+        objects = results;                                                                                                          
+    }
+}
+
+function draw() {
+    image(img,0, 0, 640, 420);
+
+    if (status != "") {
+        for (i = 0; i < objects.length; i++) {
+            percentage = floor(objects[i].confidence * 100);
+            fill("#FF0000");
+            text(objects[i].label + " " + percentage + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }  
+}
